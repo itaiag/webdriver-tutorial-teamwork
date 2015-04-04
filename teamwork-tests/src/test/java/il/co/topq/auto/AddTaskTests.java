0fc4f76d6f5c115805d07b132947a3490cfd9260
itaiag <itai.agmon@gmail.com>
2015-04-04 20:56:25 +0300

    sol02

---
+++ teamwork-tests/src/test/java/il/co/topq/auto/AddTaskTests.java
@@ -0,0 +1,78 @@
+package il.co.topq.auto;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AddTaskTests {
+
+	private static final String URL = "https://topq.teamwork.com";
+	private static final CharSequence USER_NAME = "fake01";
+	private static final CharSequence PASSWORD = "fake";
+	private WebDriverWait wait;
+
+	private WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+		driver = new FirefoxDriver();
+		driver.get(URL);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, 60);
+
+	}
+
+	@Test
+	public void testAddTask() {
+		// Login
+		driver.findElement(By.id("userLogin")).sendKeys(USER_NAME);
+		driver.findElement(By.id("password")).sendKeys(PASSWORD);
+		driver.findElement(By.id("ordLoginSubmitBtn")).click();
+
+		// Select the task sub menu
+		driver.findElement(By.xpath("//li[@id='tab_tasks']/a")).click();
+
+		// Add task list
+		wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//li[@id='tab_tasks' and @class='first sel']")));
+		driver.findElement(By.xpath("//li[@id='liBFOATL']/button")).click();
+		
+		// Enter new task list name
+		final String taskListName = USER_NAME + "'s Task List " + System.currentTimeMillis();
+		driver.findElement(By.id("newTaskListName")).sendKeys(taskListName);
+		driver.findElement(By.id("btnCreateTaskList")).click();
+
+		// Add new task
+		driver.findElement(By.linkText(taskListName)).click();
+		driver.findElement(By.xpath("//button[@onclick='TaskLists.AddTaskFromTasklistPage();']")).click();
+
+		final String taskName = "My Task " + System.currentTimeMillis();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='twitter-typeahead']//input[@placeholder='What needs to be done?']"))).sendKeys(taskName);
+		driver.findElement(By.xpath("//input[@value='Save my changes']")).click();
+
+		//Getting back to the tasks view by clicking again on the task sub menu
+		driver.findElement(By.xpath("//li[@id='tab_tasks']/a")).click();
+		
+		//Asserting that the new task was created
+		driver.findElement(By.xpath("//span[@class='taskName' and contains(.,'"+taskName+"')]"));
+		
+		// Signing out
+		driver.findElement(By.xpath("//*[@id='trUserPic']/a")).click();
+		driver.findElement(By.xpath("//a[@href='?action=logout']")).click();
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+}
